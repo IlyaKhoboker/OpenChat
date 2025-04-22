8d3617ba8ce3e292a7c8e1263fe5e6d73a1db337
IlyaKhoboker <iluhovski@gmail.com>
2025-04-23 02:06:16 +0300

    created redux sign in action

---
+++ src/app/providers/store/actions/authActions.js
@@ -0,0 +1,5 @@
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_IN_SUCCESS = "SIGN_IN_SUCCESS";
+export const SIGN_IN_FAILURE = "SIGN_IN_FAILURE";
+
+export const signInAction = (payload) => ({ type: SIGN_IN, payload });
