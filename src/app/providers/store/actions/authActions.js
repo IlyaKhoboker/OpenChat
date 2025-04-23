98ceef1a809e68e1cd05cee3e241c1b3d3f4e93e
IlyaKhoboker <iluhovski@gmail.com>
2025-04-23 09:50:49 +0300

    added sign out auth action

--- src/app/providers/store/actions/authActions.js
+++ src/app/providers/store/actions/authActions.js
@@ -15,8 +15,11 @@ export const FORGOT_PASSWORD_SUCCESS = "FORGOT_PASSWORD_SUCCESS";
 export const RESET_PASSWORD = "RESET_PASSWORD";
 export const RESET_PASSWORD_SUCCESS = "RESET_PASSWORD_SUCCESS";
 
+export const SIGN_OUT = "SIGN_OUT";
+
 export const signInAction = (payload) => ({ type: SIGN_IN, payload });
 export const googleSignInAction = () => ({ type: GOOGLE_SIGN_IN });
 export const signUpAction = (payload) => ({ type: SIGN_UP, payload });
 export const forgotPasswordAction = (payload) => ({ type: FORGOT_PASSWORD, payload });
 export const resetPasswordAction = (payload) => ({ type: RESET_PASSWORD, payload });
+export const signOutAction = () => ({ type: SIGN_OUT });
