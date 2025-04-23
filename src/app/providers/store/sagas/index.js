ba73e54a416ddc94fcd7dc440e53be24f241dc24
moto_moto <ustinovilia34@gmail.com>
2025-04-23 07:37:43 +0300

    added google authorization saga

--- src/app/providers/store/sagas/index.js
+++ src/app/providers/store/sagas/index.js
@@ -1,9 +1,10 @@
 import { all } from "@redux-saga/core/effects";
 import { signInSagaWatcher } from "./authSagas/signInSaga";
 import { signUpSagaWatcher } from "./authSagas/signUpSaga";
+import { googleSignInSagaWatcher } from "./authSagas/googleSignInSaga";
 import { forgotPasswordSagaWatcher } from "./authSagas/forgotPasswordSaga";
 import { resetPasswordSagaWatcher } from "./authSagas/resetPasswordSaga";
 
 export default function* rootSaga() {
-  yield all([signInSagaWatcher(), signUpSagaWatcher(), forgotPasswordSagaWatcher(), resetPasswordSagaWatcher()]);
+  yield all([signInSagaWatcher(), googleSignInSagaWatcher(), signUpSagaWatcher(), forgotPasswordSagaWatcher(), resetPasswordSagaWatcher()]);
 }
