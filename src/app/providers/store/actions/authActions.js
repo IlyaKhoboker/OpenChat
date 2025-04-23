e69fdf50803dafaaa6d1549bd3b5df84d4bb0911
moto_moto <ustinovilia34@gmail.com>
2025-04-23 07:37:13 +0300

    added google authorization action

--- src/app/providers/store/actions/authActions.js
+++ src/app/providers/store/actions/authActions.js
@@ -2,6 +2,9 @@ export const SIGN_IN = "SIGN_IN";
 export const SIGN_IN_SUCCESS = "SIGN_IN_SUCCESS";
 export const SIGN_IN_FAILURE = "SIGN_IN_FAILURE";
 
+export const GOOGLE_SIGN_IN = "GOOGLE_SIGN_IN";
+export const GOOGLE_SIGN_IN_SUCCESS = "GOOGLE_SIGN_IN_SUCCESS";
+
 export const SIGN_UP = "SIGN_UP";
 export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
 export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
@@ -13,6 +16,7 @@ export const RESET_PASSWORD = "RESET_PASSWORD";
 export const RESET_PASSWORD_SUCCESS = "RESET_PASSWORD_SUCCESS";
 
 export const signInAction = (payload) => ({ type: SIGN_IN, payload });
+export const googleSignInAction = () => ({ type: GOOGLE_SIGN_IN });
 export const signUpAction = (payload) => ({ type: SIGN_UP, payload });
 export const forgotPasswordAction = (payload) => ({ type: FORGOT_PASSWORD, payload });
 export const resetPasswordAction = (payload) => ({ type: RESET_PASSWORD, payload });
