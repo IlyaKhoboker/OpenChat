f72a7bdc165a417f0aacaa4df192e9dd63e038de
moto_moto <ustinovilia34@gmail.com>
2025-04-23 06:50:58 +0300

    added sign up route

--- src/app/providers/router/routes.js
+++ src/app/providers/router/routes.js
@@ -1,8 +1,14 @@
 import SignIn from "../../../pages/authorization/SignIn/SignIn";
+import SignUp from "../../../pages/authorization/SignUp/SignUp";
 
 export const publicRoutes = [
   {
     path: "/signIn",
     Component: SignIn,
+  },
+
+  {
+    path: "/signUp",
+    Component: SignUp,
   },
 ];
