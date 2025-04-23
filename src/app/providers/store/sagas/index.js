ab9b0594f2a2ae59a31bb5c0eff5e425dbbaeb4a
IlyaKhoboker <iluhovski@gmail.com>
2025-04-23 09:54:03 +0300

    updated sagas config

--- src/app/providers/store/sagas/index.js
+++ src/app/providers/store/sagas/index.js
@@ -4,7 +4,26 @@ import { signUpSagaWatcher } from "./authSagas/signUpSaga";
 import { googleSignInSagaWatcher } from "./authSagas/googleSignInSaga";
 import { forgotPasswordSagaWatcher } from "./authSagas/forgotPasswordSaga";
 import { resetPasswordSagaWatcher } from "./authSagas/resetPasswordSaga";
+import { signOutSagaWatcher } from "./authSagas/signOutSaga";
+
+import { createRoomSagaWatcher } from "./chatSagas/createRoomSaga";
+import { enterChatRoomSagaWatcher } from "./chatSagas/enterChatRoomSaga";
+import { sendMessageSagaWatcher } from "./chatSagas/sendMessageSaga";
+import { deleteMessageSagaWatcher } from "./chatSagas/deleteMessageSaga";
+import { deleteRoomSagaWatcher } from "./chatSagas/deleteRoomSaga";
 
 export default function* rootSaga() {
-  yield all([signInSagaWatcher(), googleSignInSagaWatcher(), signUpSagaWatcher(), forgotPasswordSagaWatcher(), resetPasswordSagaWatcher()]);
+  yield all([
+    signInSagaWatcher(),
+    googleSignInSagaWatcher(),
+    signUpSagaWatcher(),
+    signOutSagaWatcher(),
+    forgotPasswordSagaWatcher(),
+    resetPasswordSagaWatcher(),
+    createRoomSagaWatcher(),
+    enterChatRoomSagaWatcher(),
+    sendMessageSagaWatcher(),
+    deleteMessageSagaWatcher(),
+    deleteRoomSagaWatcher(),
+  ]);
 }
