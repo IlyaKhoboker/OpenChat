85079c554f8977ea0003de6ce1d55a72e8ddf83c
moto_moto <ustinovilia34@gmail.com>
2025-04-23 06:50:24 +0300

    added sign up saga

--- src/app/providers/store/sagas/index.js
+++ src/app/providers/store/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from "@redux-saga/core/effects";
 import { signInSagaWatcher } from "./authSagas/signInSaga";
+import { signUpSagaWatcher } from "./authSagas/signUpSaga";
 
 export default function* rootSaga() {
-  yield all([signInSagaWatcher()]);
+  yield all([signInSagaWatcher(), signUpSagaWatcher()]);
 }
