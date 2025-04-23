e3cc3fe07b96d034fab2caf357db4b16ba539bee
moto_moto <ustinovilia34@gmail.com>
2025-04-23 06:46:18 +0300

    added sign up action

--- src/app/providers/store/actions/authActions.js
+++ src/app/providers/store/actions/authActions.js
@@ -2,4 +2,9 @@ export const SIGN_IN = "SIGN_IN";
 export const SIGN_IN_SUCCESS = "SIGN_IN_SUCCESS";
 export const SIGN_IN_FAILURE = "SIGN_IN_FAILURE";
 
+export const SIGN_UP = "SIGN_UP";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
+
 export const signInAction = (payload) => ({ type: SIGN_IN, payload });
+export const signUpAction = (payload) => ({ type: SIGN_UP, payload });
