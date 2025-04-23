c491236b74b09f51b38e8436cf01a924a3b38060
moto_moto <ustinovilia34@gmail.com>
2025-04-23 09:50:15 +0300

    added router switch for the public and private pages

--- src/app/providers/router/AppRouter.js
+++ src/app/providers/router/AppRouter.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { publicRoutes } from "./routes";
+import { privateRoutes } from "./routes";
 
 const AppRouter = () => {
-  return (
-    <>
+  const userIsLogged = useSelector((state) => state.authReducer.userIsLogged);
+
+  return userIsLogged ? (
+    <Switch>
+      {privateRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact={true} />
+      ))}
+      <Redirect to="/main" />
+    </Switch>
+  ) : (
+    <Switch>
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} component={Component} exact={true} />
       ))}
       <Redirect to="/signIn" />
-    </>
+    </Switch>
   );
 };
 
