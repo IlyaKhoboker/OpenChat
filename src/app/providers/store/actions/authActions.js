90bb114500c685d52b0105b589c21ff38583b99a
IlyaKhoboker <iluhovski@gmail.com>
2025-04-23 07:23:55 +0300

    created password reset actions

--- src/app/providers/store/actions/authActions.js
+++ src/app/providers/store/actions/authActions.js
@@ -6,5 +6,13 @@ export const SIGN_UP = "SIGN_UP";
 export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
 export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 
+export const FORGOT_PASSWORD = "FORGOT_PASSWORD";
+export const FORGOT_PASSWORD_SUCCESS = "FORGOT_PASSWORD_SUCCESS";
+
+export const RESET_PASSWORD = "RESET_PASSWORD";
+export const RESET_PASSWORD_SUCCESS = "RESET_PASSWORD_SUCCESS";
+
 export const signInAction = (payload) => ({ type: SIGN_IN, payload });
 export const signUpAction = (payload) => ({ type: SIGN_UP, payload });
+export const forgotPasswordAction = (payload) => ({ type: FORGOT_PASSWORD, payload });
+export const resetPasswordAction = (payload) => ({ type: RESET_PASSWORD, payload });
