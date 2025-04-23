495b0ac4c9a80a505e2a65ea8cea63e9581111cf
moto_moto <ustinovilia34@gmail.com>
2025-04-23 07:28:30 +0300

    added password reser and forgot sagas

--- src/app/providers/store/sagas/index.js
+++ src/app/providers/store/sagas/index.js
@@ -1,7 +1,9 @@
 import { all } from "@redux-saga/core/effects";
 import { signInSagaWatcher } from "./authSagas/signInSaga";
 import { signUpSagaWatcher } from "./authSagas/signUpSaga";
+import { forgotPasswordSagaWatcher } from "./authSagas/forgotPasswordSaga";
+import { resetPasswordSagaWatcher } from "./authSagas/resetPasswordSaga";
 
 export default function* rootSaga() {
-  yield all([signInSagaWatcher(), signUpSagaWatcher()]);
+  yield all([signInSagaWatcher(), signUpSagaWatcher(), forgotPasswordSagaWatcher(), resetPasswordSagaWatcher()]);
 }
